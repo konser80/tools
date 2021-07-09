ba3855745cd78b456b34ac9ac6d4390ec73a2f3e
konser80 <konser80@gmail.com>
2021-07-09 14:36:26 +0700

    new smart replace

--- pathreplace.js
+++ pathreplace.js
@@ -2,22 +2,68 @@ const _ = require('lodash');
 const dayjs = require('dayjs');
 const validate = require('./validate');
 
+const REG_FULL = /\{\?.*?(\{(\/.*?\/)?\w+\.[a-z0-9_.[\]]*?}).*?}/gi;
+const REG_MINI = /\{(\/.*?\/)?(\w+\.\w.*?)\}/gi;
+
+// example:
+// this user{?, who {user.age} years old,} can do some job
+// this {? button {/do_(.+)/btn.text} action} is great
+
 // ==============================================
-function pathReplace(object, strPath, opt = { keep: false }) {
-  if (!strPath) return strPath;
-  if (typeof strPath !== 'string') return strPath;
+function objectReplace(obj, string, opt = { keep: false }) {
+  if (!string) return string;
+  if (typeof string !== 'string') return string;
+
+  let out = string;
+  let reg;
+
+  // smart replace
+  while ((reg = REG_FULL.exec(string)) !== null) {
+
+    // console.debug(`LOOP. out = ${out}`);
+    // console.debug(reg);
+    // [0] full string
+    // [1] sub-path in {}
+    // [2] sub-regex - if any
+
+    const subres = pathReplace(obj, reg[1], opt);
+
+    // if replace result is empty - replace FULL string
+    if (subres === '') {
+      out = out.replace(reg[0], '');
+    }
+    else {
+      // remove "{?" and "}"
+      let newline = reg[0].slice(2, -1);
+      // apply replace
+      newline = newline.replace(reg[1], subres);
+      out = out.replace(reg[0], newline);
+    }
+  }
+
+  // and now - simple pathReplace
+  out = pathReplace(obj, out, opt);
+
+  // double lines & spaces
+  out = out.replace(/ +/g, ' ');
+  out = out.replace(/\n{3,}/gm, '\n\n');
+
+  return out;
+}
+
+// ==============================================
+function pathReplace(object, strPath, opt) {
   // opt:keep means keep notfound unchanged
 
   // const regex = /{(.+?)}/gi;
-  const regex = /\{(\/.*?\/)?(\w+\.\w.*?)\}/gi;
-  if (!strPath.match(regex)) return strPath;
+  if (!strPath.match(REG_MINI)) return strPath;
   // example: {/\d{3,}/msg.text}
 
   const na = 'undefined';
   let res = strPath;
   let regexResult;
 
-  while ((regexResult = regex.exec(strPath)) !== null) {
+  while ((regexResult = REG_MINI.exec(strPath)) !== null) {
     const strfull = regexResult[0];
     const sregex = regexResult[1];
     const objpath = regexResult[2];
@@ -39,7 +85,7 @@ function pathReplace(object, strPath, opt = { keep: false }) {
     if (replaceText !== na) res = res.replace(strfull, replaceText);
     if (replaceText === na && !opt.keep) res = res.replace(strfull, '');
   }
-  return res.replace(/ +/g, ' ');
+  return res;
 }
 
-module.exports = pathReplace;
+module.exports = objectReplace;
