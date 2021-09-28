69c587196a2bf5d4487c1216a7f5f55327bab32d
konser80 <konser80@gmail.com>
2021-09-28 21:09:01 +0700

    queue & timeout

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const consolemodule = require('./console');
 const validate = require('./validate');
+const queuelib = require('./queue');
 require('./async');
 
 consolemodule.init();
@@ -8,6 +9,9 @@ module.exports.val = require('./value');
 module.exports.combine = require('./combine');
 module.exports.replace = require('./pathreplace');
 module.exports.tftotime = require('./timeframes');
+
+module.exports.queue = queuelib.queue;
+module.exports.timeout = queuelib.timeout;
 
 module.exports.istime = validate.isTime;
 module.exports.isdate = validate.isDate;
---
+++ queue.js
@@ -0,0 +1,31 @@
+const dayjs = require('dayjs');
+
+const qline = {};
+
+// ==============================================
+async function queue(RPS = 10, key = 'main') {
+  const pause = Math.ceil(1000/RPS);
+
+  qline[key] = qline[key] || dayjs();
+  const diff = dayjs().diff(qline[key], 'ms');
+
+  // do it now
+  if (diff > pause) {
+    qline[key] = dayjs();
+    return true;
+  }
+
+  // wait for your time
+  qline[key] = qline[key].add(pause, 'ms');
+  const wait = pause - diff;
+  await timeout(wait);
+  return true;
+}
+
+// ==============================================
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+module.exports.queue = queue;
+module.exports.timeout = timeout;
