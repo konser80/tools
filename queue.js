fcf05dc6f20c25b02e9a36b7fe3d591a2b4b5f64
konser80 <konser80@gmail.com>
2022-01-31 19:05:23 +0200

    2.3.7 queue fixed for first call

--- queue.js
+++ queue.js
@@ -6,8 +6,9 @@ const qline = {};
 async function queue(RPS = 10, key = 'main') {
   const pause = Math.ceil(1000/RPS);
 
-  qline[key] = qline[key] || dayjs();
+  qline[key] = qline[key] || dayjs().add(-pause, 'ms');
   const diff = dayjs().diff(qline[key], 'ms');
+  // console.log(`diff ${diff} pause ${pause}`);
 
   // do it now
   if (diff > pause) {
