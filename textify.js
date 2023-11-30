8e5ac43df7dfdda92d025cf04f39e1d23f4c0bce
konser80 <konser80@gmail.com>
2023-11-30 17:22:55 +0700

    2.7.3 textify color limit fixed

--- textify.js
+++ textify.js
@@ -23,19 +23,24 @@ function textify(obj, _opt) {
     return dayjs(obj).format('YYYY-MM-DD HH:mm:ss.SSS');
   }
 
-
-  // some other object?
-  if (typeof obj !== 'string') {
-    res = util.inspect(obj, { colors: opt.colors, depth: null, showHidden: false });
-    if (opt.crlf === false) {
-      res = res.replace(/\n/g, ' ').replace(/ {2,}/g, ' ');
-    }
-  }
-
   // limit chars
   if ((opt.limit) && res.length > opt.limit) {
     res = res.slice(0, opt.limit);
     res += '...';
+  }
+
+  // some other object?
+  if (typeof obj !== 'string') {
+    const params = {
+      colors: opt.colors,
+      depth: null,
+      showHidden: false
+    };
+    res = util.inspect(obj, params);
+    if (opt.crlf === false) {
+      res = res.replace(/\n/g, ' ').replace(/ {2,}/g, ' ');
+    }
+    res = truncateColoredString(res, opt.limit);
   }
 
   return res;
@@ -68,6 +73,62 @@ function getType(src) {
   return res;
 }
 
+// ==============================================
+function truncateColoredString(inputString, maxLength) {
+  if (!maxLength) return inputString;
+  if (inputString.length <= maxLength) return inputString;
+  if (stripAnsi(inputString).length <= maxLength) return inputString;
+
+  let nonAnsiCharCount = 0;
+  let truncatedString = '';
+  let ansiCodeBuffer = '';
+
+  for (let i = 0; i < inputString.length; i += 1) {
+    const char = inputString.charAt(i);
+    const charCode = inputString.charCodeAt(i);
+
+    // start of ANSI-sequence found
+    if (charCode === 27) {
+      ansiCodeBuffer = char;
+      i += 1;
+
+      while (i < inputString.length) {
+        const nextChar = inputString.charAt(i);
+        // const nextCharCode = inputString.charCodeAt(i);
+
+        ansiCodeBuffer += nextChar;
+        i += 1;
+
+        // end of ANSI-sequence found
+        if ((nextChar >= 'A' && nextChar <= 'Z') || (nextChar >= 'a' && nextChar <= 'z')) {
+          truncatedString += ansiCodeBuffer;
+          i -= 1;
+          break;
+        }
+      }
+    }
+    else {
+      nonAnsiCharCount += 1;
+      truncatedString += char;
+
+      if (nonAnsiCharCount >= maxLength) break;
+    }
+  }
+
+  return `${truncatedString}...`;
+}
+
+// ==============================================
+function stripAnsi(string) {
+  const pattern = [
+    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
+  ].join('|');
+
+  const regex = new RegExp(pattern, 'g');
+
+  return string.replace(regex, '');
+}
 
 module.exports.typeof = getType;
 module.exports.textify = textify;
