881496bfdedcb4931c326813352f58584e3685d3
konser80 <konser80@gmail.com>
2024-08-10 15:44:04 +0400

    combine rewrite

--- combine.js
+++ combine.js
@@ -1,58 +1,48 @@
-// =============================================================
-function combine(newObject) {
-  let res = [];
+// ==============================================
+function combine(params) {
+  const keys = Object.keys(params);
+  return combineRecursive(keys, keys, 0, {}, params);
+}
 
-  const key = getFirstArrayKey(newObject);
-  if (!key) return [newObject];
-
-  // key is an array?
-  if (Array.isArray(newObject[key])) {
-    newObject[key].forEach((item) => {
-      // create copy of object
-      const obj = { ...newObject };
-      obj[key] = item;
-
-      res = res.concat(combine(obj));
-      // return res;
-    });
-  } // if array
-
-  // if pseudo-array
-  const o = newObject[key];
-  if (o.from !== undefined && o.to !== undefined) {
-    let _from = o.from;
-    if (typeof (_from) === 'string') _from = newObject[_from];
-    if (o.add) _from += o.add;
-    if (o.mult) _from *= o.mult;
-
-    let _to = o.to;
-    if (typeof (_to) === 'string') _from = newObject[_to];
-    if (o.mult) _to *= o.mult;
-
-    let _step = o.step || 1;
-    if (typeof(_step) == 'string') _step = newObject[_step];
-    if (o.mult) _step = _step * o.mult;
-
-    for (let i = _from; i <= _to;  i+= _step || 1) {
-      // create copy of object
-      let obj = Object.assign({}, newObject);
-      obj[key] = Number(i.toFixed(8));
-      res = res.concat(combine(obj));
-    };
-  }; // pseudo array
-
-  return res;
-
-  function getFirstArrayKey(obj) {
-    for (let i=0; i < Object.keys(obj).length; i++) {
-      const key = Object.keys(obj)[i];
-      if (obj[key] === null) continue;
-
-      if (Array.isArray(obj[key])
-        || (obj[key].from !== undefined && obj[key].to !== undefined)) return key;
-      }
-    return false;
+// ==============================================
+function generateRange(from, to, step = 1) {
+  const range = [];
+  for (let i = from; i <= to; i += step) {
+    range.push(i);
   }
-} // fn
+  return range;
+}
+
+// ==============================================
+function combineRecursive(arrays, keys, index = 0, current = {}, params) {
+  if (index === arrays.length) {
+    return [current];
+  }
+
+  const result = [];
+  const key = keys[index];
+  const param = params[key];
+
+  if (typeof param !== 'object') {
+    const newCombination = { ...current, [key]: param };
+    return combineRecursive(arrays, keys, index + 1, newCombination, params);
+  }
+
+  let { from } = param;
+  const { to } = param;
+  const step = param.step || 1;
+
+  // one param depends on another
+  if (typeof from === 'string') from = current[from];
+
+  const range = generateRange(from, to, step);
+
+  range.forEach((value) => {
+    const newCombination = { ...current, [key]: value };
+    result.push(...combineRecursive(arrays, keys, index + 1, newCombination, params));
+  });
+
+  return result;
+}
 
 module.exports = combine;
